bf581752258e493e9eec6e1f869873b6a14775cd
שמחה בן-דוד <sibendav@g.jct.ac.il>
2021-08-18 13:09:00 +0300

    bll dal

---
+++ BLL/BLLFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLFactory
+    {
+        public static IBll instance;
+
+        protected BLFactory() { instance = null; }
+        public static IBll getBL()
+        {
+            if (null == instance)
+                instance = new imp_bll();
+            return instance;
+        }
+    }
+
+}
---
+++ DAL/IDal.cs
@@ -0,0 +1,56 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IDal
+    {
+        bool addFamily(Family var);
+        bool updateFamily(Family var);
+        bool deleteFamily(Family var);
+
+        bool addAddress(Address var);
+        bool updateAddress(Address var);
+        bool deleteAddress(Address var);
+
+        bool addStore(Store var);
+        bool updateStore(Store var);
+        bool deleteStore(Store var);
+
+        bool addProduct(Product var);
+        bool updateProduct(Product var);
+        bool deleteProduct(Product var);
+
+        bool addCart(Cart var);
+        bool updateCart(Cart var);
+        bool deleteCart(Cart var);
+
+        bool addProductInCart(ProductInCart var);
+        bool updateProductInCart(ProductInCart var);
+        bool deleteProductInCart(ProductInCart var);
+
+        bool addQRcode(QRcode var);
+        bool updateQRcode(QRcode var);
+        bool deleteQRcode(QRcode var);
+
+        Family returnFamily(int var);
+        Address returnAddress(int var);
+        Store returnStore(int var);
+        Product returnProduct(int var);
+        Cart returnCart(int var);
+        ProductInCart returnProductInCart(int var);
+        QRcode returnQRcode(int var);
+
+        List<Family> returnAllFamily();
+        List<Address> returnAllAddress();
+        List<Store> returnAllStore();
+        List<Product> returnAllProduct();
+        List<Cart> returnAllCart();
+        List<ProductInCart> returnAllProductInCart();
+        List<QRcode> returnAllQRcode();
+    }
+}
